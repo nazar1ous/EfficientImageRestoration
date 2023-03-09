d30728b6f9e176b12b479ce5e9b45d2b36acad9e
Nazarii Kuspys <nazar0620@gmail.com>
2023-03-09 17:57:00 +0200

    fix activate issue for shell script

--- ubuntu_setup_all.sh
+++ ubuntu_setup_all.sh
@@ -1,22 +1,18 @@
 # Setup conda env
-conda create -n efnet python=3.9 -y
-conda activate efnet
-# Install pytorch
-conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia -y
-# Install req
-pip install -r requirements.txt
+conda create -n efnet python=3.9 -y && source ~/anaconda3/etc/profile.d/conda.sh && conda activate efnet &&
+conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia -y &&
+# Install requirements
+pip install -r requirements.txt &&
 # Install BasicSR package
 python setup.py develop --no_cuda_ext
 
 cd datasets/
 pip install gdown
-# Download Train data
-gdown https://drive.google.com/file/d/1zgALzrLCC_tcXKu_iHQTHukKUVT1aodI/view?usp=sharing --fuzzy
 # Download Test data
-gdown https://drive.google.com/file/d/1abXSfeRGrzj2mQ2n2vIBHtObU6vXvr7C/view?usp=sharing --fuzzy
-unzip GoPro-test-lmdb.zip
+gdown https://drive.google.com/file/d/1abXSfeRGrzj2mQ2n2vIBHtObU6vXvr7C/view?usp=sharing --fuzzy &&
+    unzip GoPro-test-lmdb.zip
 
-unzip train.zip
-mv train GoPro
+# Download Train data
 # Crop the train image pairs to 512x512 patches and make the data into lmdb format.
-python scripts/data_preparation/gopro.py
+gdown https://drive.google.com/file/d/1zgALzrLCC_tcXKu_iHQTHukKUVT1aodI/view?usp=sharing --fuzzy &&
+    unzip train.zip && pip install opencv-python tqdm && python scripts/data_preparation/gopro.py
